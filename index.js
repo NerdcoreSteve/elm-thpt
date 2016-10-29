086b0ba850a7c2073e9ab6e9df8e8a905aa25909
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-10-29 13:05:22 -0600

    got elm working

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var express = require('express')  
 var app = express()  
-app.set('view engine', 'pug')
 app.use('/public', express.static('public'))
 
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function (req, res) {  
-    res.render('index')
+    res.sendfile('index.html')
 })
 
 app.listen(app.get('port'), function() {
